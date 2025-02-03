b45bbe24a2e4d0daae323de6373a46c06225bd28
Stephen-ONeil <stephenlevioneil@gmail.com>
2025-02-03 15:27:21 -0500

    Add init express project setup for the federator API, add initial configuration to the docker-compose environment

---
+++ federator/src/create_app.test.ts
@@ -0,0 +1,11 @@
+import { create_app } from './create_app.ts';
+
+describe('create_app', () => {
+  describe('TODO', () => {
+    it('TODO', async () => {
+      const app = await create_app();
+
+      expect(true).toEqual(true);
+    });
+  });
+});
