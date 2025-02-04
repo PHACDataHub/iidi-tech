a591fae4db9797c02598bc51ac6e5ab61ad00b3b
Stephen-ONeil <stephenlevioneil@gmail.com>
2025-02-04 11:52:43 -0500

    Make chainges in /federator to satisfy the typechecker. Add a basic healthcheck route and test while I'm at it, and some jest configuration changes to fix aminor coverage collection error

--- federator/src/create_app.test.ts
+++ federator/src/create_app.test.ts
@@ -1,11 +1,15 @@
+import request from 'supertest';
+
 import { create_app } from './create_app.ts';
 
 describe('create_app', () => {
-  describe('TODO', () => {
-    it('TODO', async () => {
+  describe('/healthcheck', () => {
+    it('Returns a 200 when the server is running', async () => {
       const app = await create_app();
 
-      expect(true).toEqual(true);
+      const response = await request(app).get('/healthcheck').send();
+
+      expect(response.statusCode).toEqual(200);
     });
   });
 });
