9add4b928ea360aca38160a5012cff2cbeea0649
Stephen-ONeil <stephenlevioneil@gmail.com>
2025-02-04 11:52:43 -0500

    Make chainges in /federator to satisfy the typechecker. Add a basic healthcheck route and test while I'm at it, and some jest configuration changes to fix aminor coverage collection error

--- federator/src/setup_tests.ts
+++ federator/src/setup_tests.ts
@@ -2,3 +2,7 @@ import dotenv from 'dotenv';
 
 dotenv.config({ path: '.env.dev-public' }); // relative to the call point, e.g. the service root
 process.env.DEV_IS_TEST_ENV = 'true';
+
+// placeholder so env.ts' validation passes by default. Individual tests that depend on a specific AGGREGATOR_URLS
+// value should override/revert as a pre and post test step
+process.env.AGGREGATOR_URLS = 'http://localhost:8080/placeholder/';
--- transfer-inbound/src/create_app.test.ts
+++ transfer-inbound/src/create_app.test.ts
@@ -3,7 +3,7 @@ import { create_app } from './create_app.ts';
 describe('create_app', () => {
   describe('TODO', () => {
     it('TODO', async () => {
-      const app = await create_app();
+      //const app = await create_app();
 
       expect(true).toEqual(true);
     });
