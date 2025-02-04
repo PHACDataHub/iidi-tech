c1d07c84a37db504c2c99646e79626d6fc9cb6b3
Stephen-ONeil <stephenlevioneil@gmail.com>
2025-02-04 13:31:25 -0500

    Repeat applicable cleanup from /federator in /transfer-inbound and /transfer-outbound

--- transfer-outbound/jest.config.cjs
+++ transfer-outbound/jest.config.cjs
@@ -3,14 +3,20 @@ const { pathsToModuleNameMapper } = require('ts-jest');
 const { compilerOptions } = require('./tsconfig.json');
 
 module.exports = {
-  // api specific
+  transform: {
+    '.*ts': [
+      'ts-jest',
+      {
+        // so that ts-jest collects coverage without complaining about ESM features being used
+        useESM: true,
+      },
+    ],
+  },
   testEnvironment: 'node',
   preset: 'ts-jest',
   setupFilesAfterEnv: ['./src/setup_tests.ts'],
   collectCoverageFrom: ['src/**/*.ts'],
   coveragePathIgnorePatterns: ['.test.ts'],
-
-  // common with /ui jest config
   roots: ['<rootDir>'],
   modulePaths: [compilerOptions.baseUrl],
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
