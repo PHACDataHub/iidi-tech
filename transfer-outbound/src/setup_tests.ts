9ba80e6a0b488565ad08ee30e8a97ca9383fc4c1
Stephen-ONeil <stephenlevioneil@gmail.com>
2025-02-06 15:54:29 -0500

    Add FHIR_URL env var to transfer-inbound and transfer-outbound

--- transfer-outbound/src/setup_tests.ts
+++ transfer-outbound/src/setup_tests.ts
@@ -1,4 +1,6 @@
 import dotenv from 'dotenv';
 
 dotenv.config({ path: '.env.node-dev-docker-env-overrides' }); // relative to the call point, e.g. the service root
+
 process.env.DEV_IS_TEST_ENV = 'true';
+process.env.FHIR_URL = 'https://placeholder/fhir';
