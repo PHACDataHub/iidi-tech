e3e2ad76a366b0eb6bfb4a1a26f46fbd8f4b03d9
Stephen-ONeil <stephenlevioneil@gmail.com>
2025-02-07 15:35:17 -0500

    Add skeleton of a possible transfer-outbound service API

--- transfer-outbound/src/create_app.ts
+++ transfer-outbound/src/create_app.ts
@@ -4,12 +4,9 @@ import { get_env } from './env.ts';
 import { expressErrorHandler } from './error_utils.ts';
 
 export const create_app = async () => {
-  const { DEV_IS_LOCAL_ENV } = get_env();
+  const { FHIR_URL } = get_env();
 
-  // TODO delete later, just here so the TS compiler doesn't complain about DEV_IS_LOCAL_ENV being unused.
-  // I want the get_env import and usage demonstrated here, which is why I'm leaving it in when it's not
-  // yet used
-  console.log(`DEV_IS_LOCAL_ENV: ${DEV_IS_LOCAL_ENV}`);
+  console.log(FHIR_URL); // TODO delete later, just here to stop TS compiler complaining about unused var
 
   const app = express();
 
@@ -24,7 +21,49 @@ export const create_app = async () => {
     res.status(200).send();
   });
 
-  // TODO service-specific-endpoints-go-here
+  /* transferRequest type to look something like:
+    {
+      id: string, // unique ID of the transferRequests entity
+      patient: patientId,
+      transfer_to: inboundPtId, // for the demo state, "ON" | "BC", minus the Id of the province this outbound API belongs to
+      // `stage` TBD, currently thinking to use a task queue for processing with stages along these lines
+      // Using a proper queue and processing microservices could be a follow up time permitting, but handle it in-server for now
+      stage: "initializing" | "collecting" | "marking_transfered" | "transfering" | "done" | "rejecting" | "rejected"
+      stage_history: [stage], // array of prior stage values for this transfer. Used for things like, when rejecting, knowing if transfer marks need to be reverted
+      collected_records?: string, // stringified JSON blob of records to be transfered, 
+      rejection_message?: string, // error that caused the rejection
+    }
+  */
+
+  app.get('/transferRequest/', (_req, res) => {
+    // TODO get all transferRequests
+
+    // should allow query params for pagination and filtering by live/finished
+
+    res.status(501).send();
+  });
+
+  app.get('/transferRequest/:transferRequestId', (_req, res) => {
+    // TODO get transferRequest by ID
+
+    res.status(501).send();
+  });
+
+  app.get('/patient/:patientId/transferRequest', (_req, res) => {
+    // TODO get all transferRequests for a patient
+
+    // should allow query params for pagination and filtering by live/finished
+
+    res.status(501).send();
+  });
+
+  app.post('/patient/:patientId/transferRequest', (_req, res) => {
+    // TODO create new transferRequest for a patient
+
+    // should be assumed to only accept a subset of the transferRequest spec, just `patient` and `transfer_to`
+
+    res.status(501).send();
+  });
 
   app.use(expressErrorHandler);
 
