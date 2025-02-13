b609880210fefd095de12a833f6b617da2e0d5ba
Stephen-ONeil <stephenlevioneil@gmail.com>
2025-02-13 10:44:41 -0500

    For each outbound transfer container in the docker compose environment, add redis containers to back BullMQ job queues. Start sketching out a basic queue and worker based transferRequest processing sequence

--- transfer-outbound/src/create_app.ts
+++ transfer-outbound/src/create_app.ts
@@ -1,13 +1,9 @@
 import express from 'express';
 
-import { get_env } from './env.ts';
 import { expressErrorHandler } from './error_utils.ts';
+import { initialize_transfer_request } from './transfer_request_utils.ts';
 
 export const create_app = async () => {
-  const { FHIR_URL } = get_env();
-
-  console.log(FHIR_URL); // TODO delete later, just here to stop TS compiler complaining about unused var
-
   const app = express();
 
   // we'll need to be able to read `X-Forwarded-*` headers, both in prod and when using the dev docker setup
@@ -21,26 +17,23 @@ export const create_app = async () => {
     res.status(200).send();
   });
 
-  /* transferRequest type to look something like:
-    {
-      id: string, // unique ID of the transferRequests entity
-      patient: patientId,
-      transfer_to: inboundPtId, // for the demo state, "ON" | "BC", minus the Id of the province this outbound API belongs to
-      // `stage` TBD, currently thinking to use a task queue for processing with stages along these lines
-      // Using a proper queue and processing microservices could be a follow up time permitting, but handle it in-server for now
-      stage: "initializing" | "collecting" | "marking_transfered" | "transfering" | "done" | "rejecting" | "rejected"
-      stage_history: [stage], // array of prior stage values for this transfer. Used for things like, when rejecting, knowing if transfer marks need to be reverted
-      collected_records?: string, // stringified JSON blob of records to be transfered, 
-      rejection_message?: string, // error that caused the rejection
-    }
-  */
-
-  app.get('/transferRequest/', (_req, res) => {
+  app.get('/transferRequest', (_req, res) => {
     // TODO get all transferRequests
 
     // should allow query params for pagination and filtering by live/finished
 
     res.status(501).send();
+  });
+
+  app.post('/transferRequest', async (req, res) => {
+    const { patient_id, transfer_to } = req.body;
+
+    const transfer_request = await initialize_transfer_request(
+      patient_id,
+      transfer_to,
+    );
+
+    res.status(200).type('json').send(transfer_request);
   });
 
   app.get('/transferRequest/:transferRequestId', (_req, res) => {
@@ -53,14 +46,6 @@ export const create_app = async () => {
     // TODO get all transferRequests for a patient
 
     // should allow query params for pagination and filtering by live/finished
-
-    res.status(501).send();
-  });
-
-  app.post('/patient/:patientId/transferRequest', (_req, res) => {
-    // TODO create new transferRequest for a patient
-
-    // should be assumed to only accept a subset of the transferRequest spec, just `patient` and `transfer_to`
 
     res.status(501).send();
   });
---
+++ transfer-outbound/src/job_queue/get_queue.ts
@@ -0,0 +1,24 @@
+import { Queue } from 'bullmq';
+
+import _ from 'lodash';
+
+import { get_env } from 'src/env.ts';
+
+const get_queue_cached = _.memoize(
+  (queue_name: string, host: string, port: number, password: string) =>
+    new Queue(queue_name, {
+      connection: {
+        host,
+        port,
+        password,
+      },
+    }),
+  (queue_name: string, host: string, port: number, password: string) =>
+    `${queue_name}-${host}-${port}-${password}`,
+);
+
+export const get_queue = (queue_name = 'transfer-request-queue') => {
+  const { REDIS_PORT, REDIS_PASSWORD, REDIS_HOST } = get_env();
+
+  return get_queue_cached(queue_name, REDIS_HOST, REDIS_PORT, REDIS_PASSWORD);
+};
