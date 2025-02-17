631f6395fa4424a099f72366d5507e3b9c3a4509
Stephen-ONeil <stephenlevioneil@gmail.com>
2025-02-17 18:12:42 -0500

    Drop fhirclient dependency from transfer-inbound, add @types/fhir. Gave the fhirclient a test while implementing the patient validation logic on the transfer-outbound side and it wasn't worth the trouble for now (module export didn't match the docs or types)

--- transfer-inbound/src/fhir_utils.ts
+++ transfer-inbound/src/fhir_utils.ts
@@ -1,18 +1,8 @@
-import FHIR from 'fhirclient';
-
 import { get_env } from './env.ts';
 import { AppError } from './error_utils.ts';
 
-const get_fhir_client = () => {
-  const { FHIR_URL } = get_env();
-  return FHIR.client(FHIR_URL);
-};
-
 export const assert_bundle_follows_fhir_spec = async (_bundle: unknown) => {
-  // TODO: the fhirclient library looked useful at a glance, but if it isn't making things easier
-  // then drop and use raw fetch
-  const fhir_client = get_fhir_client(); // https://docs.smarthealthit.org/client-js/client
-
+  const { FHIR_URL } = get_env();
   try {
     // TODO implement bundle fhir spec validation here
     // https://build.fhir.org/resource-operation-validate.html
@@ -21,7 +11,7 @@ export const assert_bundle_follows_fhir_spec = async (_bundle: unknown) => {
 
     // Throw a descriptive AppError if validation fails
 
-    await fhir_client.request('TODO');
+    await fetch(`${FHIR_URL}/TODO");`);
   } catch (err) {
     if (err instanceof Error && !('status' in err)) {
       throw new AppError(500, err.message);
@@ -32,16 +22,13 @@ export const assert_bundle_follows_fhir_spec = async (_bundle: unknown) => {
 };
 
 export const write_bundle_to_fhir_api = async (_bundle: unknown) => {
-  // TODO: the fhirclient library looked useful at a glance, but if it isn't making things easier
-  // then drop and use raw fetch
-  const fhir_client = get_fhir_client(); // https://docs.smarthealthit.org/client-js/client
-
+  const { FHIR_URL } = get_env();
   try {
     // TODO implement bundle writing here
     // https://build.fhir.org/bundle.html
     // https://hl7.org/fhir/http.html#transaction
 
-    await fhir_client.request('TODO');
+    await fetch(`${FHIR_URL}/TODO");`);
   } catch (err) {
     if (err instanceof Error && !('status' in err)) {
       throw new AppError(500, err.message);
