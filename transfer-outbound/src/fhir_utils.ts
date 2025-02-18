eeb87056d040c3ce8623067527ac6b0c452b1a1d
Stephen-ONeil <stephenlevioneil@gmail.com>
2025-02-18 10:56:16 -0500

    Remove redundant try/catch blocks in inbound and outbound fhir utils, the express error handler already converts non-AppError errors to status code 500 responses

--- transfer-outbound/src/fhir_utils.ts
+++ transfer-outbound/src/fhir_utils.ts
@@ -14,44 +14,36 @@ export const assert_patient_exists_and_is_untransfered = async (
 ) => {
   const { FHIR_URL } = get_env();
 
-  try {
-    const response = await fetch(`${FHIR_URL}/Patient/${patient_id}`);
-    const json = await response.json();
+  const response = await fetch(`${FHIR_URL}/Patient/${patient_id}`);
+  const json = await response.json();
 
-    if (response.status === 200) {
-      if (is_patient_resource(json)) {
-        const patient_transfered_to = json.extension?.find(
-          ({ url }) =>
-            url ===
-            'TODO if we want to store transfer status as an extension, we need to publish the spec def at a known url',
-        )?.valueString;
+  if (response.status === 200) {
+    if (is_patient_resource(json)) {
+      const patient_transfered_to = json.extension?.find(
+        ({ url }) =>
+          url ===
+          'TODO if we want to store transfer status as an extension, we need to publish the spec def at a known url',
+      )?.valueString;
 
-        if (patient_transfered_to !== undefined) {
-          throw new AppError(
-            400,
-            `Patient "${patient_id}" exists, but has already been transfered out to "${patient_transfered_to}"`,
-          );
-        }
-      } else {
+      if (patient_transfered_to !== undefined) {
         throw new AppError(
-          500,
-          `FHIR server returned a non-Patient resource for id "${patient_id}". This should never happen`,
+          400,
+          `Patient "${patient_id}" exists, but has already been transfered out to "${patient_transfered_to}"`,
         );
       }
     } else {
-      const fhir_diagnostic_messages = (
-        json as { issue?: [{ diagnostics?: string }] }
-      )?.issue
-        ?.map(({ diagnostics }) => diagnostics)
-        .join(', ');
+      throw new AppError(
+        500,
+        `FHIR server returned a non-Patient resource for id "${patient_id}". This should never happen`,
+      );
+    }
+  } else {
+    const fhir_diagnostic_messages = (
+      json as { issue?: [{ diagnostics?: string }] }
+    )?.issue
+      ?.map(({ diagnostics }) => diagnostics)
+      .join(', ');
 
-      throw new AppError(response.status, fhir_diagnostic_messages ?? '');
-    }
-  } catch (err) {
-    if (err instanceof Error && !('status' in err)) {
-      throw new AppError(500, err.message);
-    } else {
-      throw err;
-    }
+    throw new AppError(response.status, fhir_diagnostic_messages ?? '');
   }
 };
