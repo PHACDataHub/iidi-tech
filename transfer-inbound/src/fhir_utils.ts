969044b927d98af4a3ce64ecbd2994918a14914b
Stephen-ONeil <stephenlevioneil@gmail.com>
2025-02-18 10:56:16 -0500

    Remove redundant try/catch blocks in inbound and outbound fhir utils, the express error handler already converts non-AppError errors to status code 500 responses

--- transfer-inbound/src/fhir_utils.ts
+++ transfer-inbound/src/fhir_utils.ts
@@ -3,44 +3,28 @@ import { AppError } from './error_utils.ts';
 
 export const assert_bundle_follows_fhir_spec = async (_bundle: unknown) => {
   const { FHIR_URL } = get_env();
-  try {
-    // TODO implement bundle fhir spec validation here
-    // Throw a descriptive AppError if validation fails
+  // TODO implement bundle fhir spec validation here
+  // Throw a descriptive AppError if validation fails
 
-    // References:
-    // https://build.fhir.org/resource-operation-validate.html
-    // https://build.fhir.org/bundle.html
-    // https://hl7.org/fhir/http.html#transaction
+  // References:
+  // https://build.fhir.org/resource-operation-validate.html
+  // https://build.fhir.org/bundle.html
+  // https://hl7.org/fhir/http.html#transaction
 
-    throw new AppError(501, 'FHIR spec validation not implemented');
+  throw new AppError(501, 'FHIR spec validation not implemented');
 
-    await fetch(`${FHIR_URL}/TODO`);
-  } catch (err) {
-    if (err instanceof Error && !('status' in err)) {
-      throw new AppError(500, err.message);
-    } else {
-      throw err;
-    }
-  }
+  await fetch(`${FHIR_URL}/TODO`);
 };
 
 export const write_bundle_to_fhir_api = async (_bundle: unknown) => {
   const { FHIR_URL } = get_env();
-  try {
-    // TODO implement bundle writing here
+  // TODO implement bundle writing here
 
-    // References:
-    // https://build.fhir.org/bundle.html
-    // https://hl7.org/fhir/http.html#transaction
+  // References:
+  // https://build.fhir.org/bundle.html
+  // https://hl7.org/fhir/http.html#transaction
 
-    throw new AppError(501, 'Writing to FHIR API not implemented');
+  throw new AppError(501, 'Writing to FHIR API not implemented');
 
-    await fetch(`${FHIR_URL}/TODO`);
-  } catch (err) {
-    if (err instanceof Error && !('status' in err)) {
-      throw new AppError(500, err.message);
-    } else {
-      throw err;
-    }
-  }
+  await fetch(`${FHIR_URL}/TODO`);
 };
