e328c0de499be4812f8d801acbaa2a8752143c5e
Stephen-ONeil <stephenlevioneil@gmail.com>
2025-02-18 16:04:05 -0500

    Correct type casing of transferCode type name

--- transfer-outbound/src/transfer_request_queue/transferRequest.d.ts
+++ transfer-outbound/src/transfer_request_queue/transferRequest.d.ts
@@ -1,4 +1,4 @@
-import type { TransferCode } from 'src/types.d.ts';
+import type { transferCode } from 'src/types.d.ts';
 
 export type transferStage =
   | 'pending'
@@ -11,7 +11,7 @@ export type transferStage =
 
 export interface transferRequest {
   patient_id: string;
-  transfer_to: TransferCode;
+  transfer_to: transferCode;
   // `stage` TBD, currently thinking to use a task queue for processing with stages along these lines
   // Using a proper task queue and persistence mechanism, maybe separate microservices for processing, could be a follow up time permitting,
   // but handle it all in-memory for initial minimal viable implementation
