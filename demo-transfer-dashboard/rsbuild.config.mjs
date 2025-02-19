e06c7d7f5ce554cf79a07c8e93245080da99a70f
Stephen-ONeil <stephenlevioneil@gmail.com>
2025-02-19 16:20:33 -0500

    Use rspack source.define config to inject ON and BC outbound transfer API urls in to builds. Also add *.mjs to the eslint config block for node script files

--- demo-transfer-dashboard/rsbuild.config.mjs
+++ demo-transfer-dashboard/rsbuild.config.mjs
@@ -3,4 +3,14 @@ import { pluginReact } from '@rsbuild/plugin-react';
 
 export default defineConfig({
   plugins: [pluginReact()],
+  source: {
+    define: {
+      'process.env.ON_OUTBOUND_URL': JSON.stringify(
+        process.env.ON_OUTBOUND_URL,
+      ),
+      'process.env.BC_OUTBOUND_URL': JSON.stringify(
+        process.env.BC_OUTBOUND_URL,
+      ),
+    },
+  },
 });
