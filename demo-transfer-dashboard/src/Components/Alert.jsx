b3bdd286fb6fa2e0081cb512b9e6375844ad395a
sami <atoui79@gmail.com>
2025-02-19 12:15:45 -0600

    french disclaimer

--- demo-transfer-dashboard/src/Components/Alert.jsx
+++ demo-transfer-dashboard/src/Components/Alert.jsx
@@ -7,12 +7,16 @@ const Alert = () => {
       <GcdsNotice
         type="info"
         noticeTitleTag="h2"
-        noticeTitle="Work in progress"
+        noticeTitle="Work in progress / Travail en cours"
       >
         <GcdsText>
-          {' '}
           This system is for synthetic data testing purposes only for IIDI
           project. No real patient data is used.
+        </GcdsText>
+        <GcdsText>
+          Ce système est destiné uniquement à des fins de test de données
+          synthétiques pour le projet IIDI. Aucune donnée réelle de patient
+          n'est utilisée.
         </GcdsText>
       </GcdsNotice>
     </div>
