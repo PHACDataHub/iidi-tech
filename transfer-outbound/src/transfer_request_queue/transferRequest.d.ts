5cf4e864c9244bbf9cfada2d039140880449cbe7
Stephen-ONeil <stephenlevioneil@gmail.com>
2025-02-21 16:38:21 -0500

    Remove old comment in transferRequest.d.ts

--- transfer-outbound/src/transfer_request_queue/transferRequest.d.ts
+++ transfer-outbound/src/transfer_request_queue/transferRequest.d.ts
@@ -5,9 +5,6 @@ import type { transferStage } from './transfer_stage_utils.ts';
 export interface transferRequest {
   patient_id: string;
   transfer_to: transferCode;
-  // `stage` TBD, currently thinking to use a task queue for processing with stages along these lines
-  // Using a proper task queue and persistence mechanism, maybe separate microservices for processing, could be a follow up time permitting,
-  // but handle it all in-memory for initial minimal viable implementation
   stage: transferStage;
   stage_history: transferStage[];
 }
