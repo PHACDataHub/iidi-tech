c4ad4865e7fd303f4213a7f2856c3961295808e5
Stephen-ONeil <stephenlevioneil@gmail.com>
2025-02-21 17:24:52 -0500

    Add a bunch of TODO comments in transfer_worker.ts, to bookmark my thoughts over the weekend

--- transfer-outbound/src/transfer_request_queue/transfer_worker.ts
+++ transfer-outbound/src/transfer_request_queue/transfer_worker.ts
@@ -45,6 +45,8 @@ const work_on_transfer_job = async (job: transferRequestJob) => {
       );
     } else if (job.data.stage === 'transfering') {
       // TODO POST bundle to transfer-inbound
+      // NOTE: vital assumption: this end point returns 200 IF AND ONLY IF the bundle was accepted and fully written to the receiving
+      // FHIR server. Not a big ask, just have to hold to that or else the rollback handling won't work and data integrity is lost
     } else if (job.data.stage === 'finalizing') {
       // TODO potentially circle back to add final confirmation mark, maybe the patient ID in the inbound system, to
       // the patient record in the outbound system
@@ -95,14 +97,25 @@ export const initialize_transfer_worker = () => {
     connection: get_connection_options(),
   });
   queueEvents.on('failed', async ({ jobId }) => {
+    // TODO confirm understanding of queue level failed events. If I understand correctly, this is esentially a worker on a
+    // queue for global failure messages, so we should have all the resiliancy benefits of a queue behind this cleanup stage
+    // ... but what if the handler of a failure event fails? Does it still get three attempts? Does it trigger a second-level
+    // `failed` event we might have to handle here?
+    // Also, as noted below, is is treated as a child of the original failed job or otherwise prevent the failed job from
+    // being pruned from the failed queue? Recovery requires that we can get the state of the failing ID still, but this event
+    // only receives its ID, so hopefully we can still look it up
+
     const failed_job = await get_transfer_request_by_id(jobId);
 
     if (failed_job === undefined) {
-      // TODO want to ensure this never happens!
+      // TODO want to ensure this never happens! Look further in to how the failed events are processed,
+      // whether a failed job can be pruned before it's failed event is complete
       console.error(
-        `Unable to ensure transfer state rollback for failed event "${jobId}", no such job found`,
+        `Unable to ensure possible intermediate state is rolled back for failed event "${jobId}", no such job found`,
       );
     } else {
+      // TODO confirm this failed job is actually a transfer request job before continuing with rollback attempt
+
       const info = await get_transfer_request_job_info(failed_job);
       console.log(JSON.stringify(info, null, 2));
 
