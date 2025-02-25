32796737059cc393d3ccb8d594efb3331046a2b5
Stephen-ONeil <stephenlevioneil@gmail.com>
2025-02-25 11:14:11 -0500

    Split transfer outbound marking of transfered patients in to two steps, one before transfering occurs (for book keeping, can be rolled back), and one once transfering has occured (to confirm the transfer and, possibly, to note the new id of the patient in the receiving system)

--- transfer-outbound/src/fhir_utils.ts
+++ transfer-outbound/src/fhir_utils.ts
@@ -20,7 +20,7 @@ export const assert_patient_exists_and_is_untransfered = async (
 
   if (response.status === 200) {
     if (is_patient_resource(json)) {
-      // TODO transfer mark storage specific TBD, see mark_patient_transfered method
+      // TODO transfer mark storage specific TBD, see mark_patient_transfering method
       const patient_transfered_to = json.extension?.find(
         ({ url }) =>
           url ===
@@ -62,30 +62,27 @@ export const get_patient_bundle_for_transfer = async (_patient_id: string) => {
   await fetch(`${FHIR_URL}/TODO`);
 };
 
-export const mark_patient_transfered = async (
+export const mark_patient_transfering = async (
   _patient_id: string,
   _transfer_to: transferCode,
   _transfer_request_id?: string,
 ) => {
   const { FHIR_URL } = get_env();
 
-  // TODO mark patient as transfered out in province FHIR server,
+  // TODO mark patient as being transfered out in outbound province FHIR server,
   // possibly with a FHIR spec extension field for the inbound province and transfer request ID,
   // expect this to happen after bundle collection step and before the bundle
   // is sent to the inbound service
 
-  // related note: maybe we'll want a post transfer step to also add an extension/metadata field for the patient ID of the record
-  // created in the inbound province?
-
   throw new AppError(
     501,
-    'Patient transfer marking method not implemented yet',
+    'Patient transfering marking method not implemented yet',
   );
 
   await fetch(`${FHIR_URL}/TODO`);
 };
 
-export const unmark_patient_transfered = async (
+export const unmark_patient_transfering = async (
   _patient_id: string,
   _transfer_to: transferCode,
   _transfer_request_id?: string,
@@ -96,7 +93,24 @@ export const unmark_patient_transfered = async (
 
   throw new AppError(
     501,
-    'Patient transfer unmarking method not implemented yet',
+    'Patient transfering unmarking method not implemented yet',
+  );
+
+  await fetch(`${FHIR_URL}/TODO`);
+};
+
+export const mark_patient_transfered = async (
+  _patient_id: string,
+  _new_patient_id?: string,
+) => {
+  const { FHIR_URL } = get_env();
+
+  // TODO mark patient as fully transfered out in province FHIR server, add id of patient in inbound system
+  // Might not want to assume the inbound province shared a new patient ID, cover case where it's undefined
+
+  throw new AppError(
+    501,
+    'Patient transfer marking method not implemented yet',
   );
 
   await fetch(`${FHIR_URL}/TODO`);
