934d7442bd4e80f88045b9e89538e4ab5796f4f3
Stephen-ONeil <stephenlevioneil@gmail.com>
2025-02-25 10:57:49 -0500

    Add missing env var placeholders to transfer-outbound/src/setup_tests.ts

--- transfer-outbound/src/setup_tests.ts
+++ transfer-outbound/src/setup_tests.ts
@@ -3,5 +3,8 @@ import dotenv from 'dotenv';
 dotenv.config({ path: '.env.node-dev-docker-env-overrides' }); // relative to the call point, e.g. the service root
 
 process.env.DEV_IS_TEST_ENV = 'true';
-process.env.FHIR_URL = 'https://placeholder/fhir';
 process.env.REDIS_PASSWORD = 'test-placeholder';
+process.env.FHIR_URL = 'https://placeholder/fhir';
+process.env.OWN_TRANSFER_CODE = 'BC';
+process.env.INBOUND_TRANSFER_SERIVCES_BY_TRANSFER_CODE =
+  '{ "ON": "http://placeholder" }';
