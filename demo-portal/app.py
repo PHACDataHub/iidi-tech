3d03d255d4b178bbdffa4efe7751e17f13ae7076
Stephen-ONeil <stephenlevioneil@gmail.com>
2025-03-07 18:51:06 -0500

    Add default_pt query param to per-province demo transfer dashboard links from the demo portal

--- demo-portal/app.py
+++ demo-portal/app.py
@@ -14,13 +14,13 @@ entities = {
         {"name": "Patient Browser", "url": os.getenv("BC_BROWSER_URL")},
         {"name": "HAPI FHIR Server", "url": os.getenv("BC_FHIR_URL")},
         {"name": "Aggregator", "url": os.getenv("BC_AGGREGATOR_URL")},
-        {"name": "Patient Transfer Dashboard", "url": os.getenv("DEMO_TRANSFER_DASHBOARD_URL")}
+        {"name": "Patient Transfer Dashboard", "url": os.getenv("BC_DEMO_TRANSFER_DASHBOARD_URL")}
     ],
     "Ontario (ON)": [
         {"name": "Patient Browser", "url": os.getenv("ON_BROWSER_URL")},
         {"name": "HAPI FHIR Server", "url": os.getenv("ON_FHIR_URL")},
         {"name": "Aggregator", "url": os.getenv("ON_AGGREGATOR_URL")},
-        {"name": "Patient Transfer Dashboard", "url": os.getenv("DEMO_TRANSFER_DASHBOARD_URL")}
+        {"name": "Patient Transfer Dashboard", "url": os.getenv("ON_DEMO_TRANSFER_DASHBOARD_URL")}
     ]
 }
 
